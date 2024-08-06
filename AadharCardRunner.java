2dbb864dc3190bf2ab2d74a80998138428e20e5f
ShashiLokesh <shashilokesh.xworkz@gmail.com>
2024-08-06 16:56:42 +0530

    adding AadharCardRunner file

---
+++ AadharCardRunner.java
@@ -0,0 +1,13 @@
+public class AadharCardRunner
+{
+ public static void main(String args[])
+ {
+  AadharCard.getAdd();
+  AadharCard.getSubtraction();
+  AadharCard.getMultiplication();
+  AadharCard.getDivision();
+ }
+
+
+
+}
