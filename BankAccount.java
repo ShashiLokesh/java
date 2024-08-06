d606290b812173f72497be628a4a5dd6b0c4e5f5
ShashiLokesh <shashilokesh.xworkz@gmail.com>
2024-08-06 17:09:11 +0530

    adding BankAccount file

---
+++ BankAccount.java
@@ -0,0 +1,17 @@
+public class BankAccount
+{
+
+static byte accountType= 2;
+ static int customerId= 12;
+ static short accountNumber=22;
+ static long balance=1233;
+ static float interestRate=12.1f;
+ static double transactionFee=22.1;
+ static char currency='S';
+ static boolean isActive=true;
+ static String bankName="hdfc";
+
+
+
+
+}
